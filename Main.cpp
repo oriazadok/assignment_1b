1c59ecb60f648e85c2813248710ea28ddcaa0a1b
oriazadok <94468518+oriazadok@users.noreply.github.com>
2022-03-15 16:36:53 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,23 @@
 #include "mat.hpp"
-// #include <iostream>
-// #include <string>
 
 int main() {
-    int columns, rows;
-    char symbol1, symbol2;
-    cout << "Enter a number for columns: ";
-    cin >> columns;
-    cout << "Enter a number for columns: ";
-    cin >> rows;
-    cout << "Enter the first symbol: ";
-    cin >> symbol1;
-    cout << "Enter the the second symbol: ";
-    cin >> symbol2;
-    cout << endl;
-
-    cout << ariel::mat(columns, rows, symbol1, symbol2);
+    
+    cout << ariel::mat(1, 1, '%', '*') << endl;
+    cout << ariel::mat(7, 1, '!', '#') << endl;
+    cout << ariel::mat(1, 7, '#', '^') << endl;
+    cout << ariel::mat(5, 3, '#', '^') << endl;
+    cout << ariel::mat(3, 5, '#', '^') << endl;
+    
+    try {
+		cout << ariel::mat(10, 5, '$', '%') << endl;
+        cout << ariel::mat(2, 10, '%', '*') << endl;
+        cout << ariel::mat(-2, -9, '$', '%') << endl;
+        cout << ariel::mat(0, 0, '$', '%') << endl;
+        cout << ariel::mat(0, 1, '%', '*') << endl;
+        cout << ariel::mat(1, 0, '%', '*') << endl;
+        cout << ariel::mat(-4, 9, '%', '*') << endl;
+        cout << ariel::mat(1, -3, '%', '*') << endl;
+	} catch (exception& ex) {
+	 	cout << "   caught exception: " << ex.what() << endl;  // should print "Mat size is always odd"
+	}
 }
